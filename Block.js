f39d1cca37cff9d72270f1c3f89a3418b6ae8c2d
rC34o7 <76730905+rC34o7@users.noreply.github.com>
2021-04-10 19:58:31 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,44 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png");
+      this.Visibility=225;
+
+      
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed<3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        /*push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+       rect(pos.x,pos.y,this.width, this.height);
+        pop();*/
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y,this.width,this.height);
+
+        }
+        else{
+          var pos= this.body.position;
+
+          World.remove(world,this.body);
+          push();
+          this.Visibility = this.Visibility -5;
+          tint (255, this.Visibility);
+          image(this.image,pos.x,pos.y, this.width, this.height);
+          pop();
+
+        }
+      
+      
+    }
+}
